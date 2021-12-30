2ee6556e1b9930596cf2113a699feb533fed1e8d
Philumena & <philumena@alex-trust.org>
2021-12-30 12:46:36 -0500

    container: fixing flickering bug on chrome

--- components/container.js
+++ components/container.js
@@ -10,8 +10,7 @@ export default function Container({ children }) {
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="fixed top-0 right-0 w-screen h-screen" style={{ background: "radial-gradient(ellipse at top, #F21B1B 18%, transparent 50%)" }} />
-      <div className="top-0 w-screen h-screen fixed" style={{ backdropFilter: "blur(20rem)", WebkitBackdropFilter: "blur(20rem)" }} />
+      <div className="fixed top-0 right-0 w-screen h-screen" style={{ background: "radial-gradient(ellipse at top, #F21B1B 30%, transparent 50%)", filter: "blur(300px)" }} />
       <Header />
       <div className="container px-8 md:px-0 mx-auto mb-16 text-white text-lg relative bg-transparent min-h-screen">
         {children}
