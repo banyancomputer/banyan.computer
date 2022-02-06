6e5289b6c1a48a4d1cba102c138a3dda3d844d79
Philumena & <philumena@alex-trust.org>
2022-02-05 16:55:15 -0800

    whitepaper: uncommenting since it seems to need it

--- pages/whitepaper.js
+++ pages/whitepaper.js
@@ -1,28 +1,28 @@
-// import { remark } from 'remark';
-// import html from 'remark-html';
-// import slug from 'remark-slug';
-// import Container from '../components/container';
-// import { TableOfContents } from '../components/toc';
+import { remark } from 'remark';
+import html from 'remark-html';
+import slug from 'remark-slug';
+import Container from '../components/container';
+import { TableOfContents } from '../components/toc';
 
-// export default function WhitePaper({ markdownString }) {
-//   return (
-//     <Container>
-//       <div className="flex">
-//         <TableOfContents/>
-//         <div className="markdown max-w-screen-lg ml-auto" dangerouslySetInnerHTML={{ __html: markdownString }}>
-//       </div>
-//     </div>
-//     </Container>
-//   );
-// }
+export default function WhitePaper({ markdownString }) {
+  return (
+    <Container>
+      <div className="flex">
+        <TableOfContents/>
+        <div className="markdown max-w-screen-lg ml-auto" dangerouslySetInnerHTML={{ __html: markdownString }}>
+      </div>
+    </div>
+    </Container>
+  );
+}
 
-// export async function getStaticProps() {
-//   const res = await fetch("https://raw.githubusercontent.com/saintceliac/not-quite-a-whitepaper/main/README.md");
-//   const post = await res.text();
-//   const markdown = await remark().use(html).use(slug).process(post);
-//   const markdownString = markdown.toString();
+export async function getStaticProps() {
+  const res = await fetch("https://raw.githubusercontent.com/saintceliac/not-quite-a-whitepaper/main/README.md");
+  const post = await res.text();
+  const markdown = await remark().use(html).use(slug).process(post);
+  const markdownString = markdown.toString();
 
-//   return {
-//     props: { markdownString }
-//   }
-// }
+  return {
+    props: { markdownString }
+  }
+}
