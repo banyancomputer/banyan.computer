b7cb1ed1fde3a1c7265132028aa9b1bbeadc83b2
Philumena & <philumena@alex-trust.org>
2022-02-05 16:52:27 -0800

    index: making it build

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Container from '../components/container'
 import 'line-awesome/dist/line-awesome/css/line-awesome.min.css'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -12,35 +13,35 @@ export default function Home() {
       <div className="flex flex-col lg:flex-row justify-between mt-16 mb-32">
         <p className="w-full pb-8 lg:pb-0 lg:pr-12 text-justify">Place assets into a Banyan DAO-controlled trust contract for that file and provide a copy of the files.</p>
         <p className="w-full pb-8 lg:pb-0 lg:pr-12 text-justify">Banyan DAO puts extra assets under management, in a combination of yield-bearing stablecoin strategies for low-risk returns and FIL/BTC perpetuals to hedge against large crypto market moves. This gets the most storage for your money.</p>
-        <p className="w-full text-justify">Banyan DAO posts (and annually renews) staked time-release bounty piñata contracts. By doing this, we incentivize upload by third parties to reputable Filecoin service providers and IPFS pinning services. Don't fret about your storage as long as the trust has money.</p>
+        <p className="w-full text-justify">Banyan DAO posts (and annually renews) staked time-release bounty piñata contracts. By doing this, we incentivize upload by third parties to reputable Filecoin service providers and IPFS pinning services. Don&apos;t fret about your storage as long as the trust has money.</p>
       </div>
-      <img src="/diagram2.png"/>
+      <Image src="/diagram2.png" width="1280" height="640"/>
       <h2>Current solutions...</h2>
       <div className="flex flex-col lg:flex-row justify-between mt-16 mb-32">
         <p className="w-full pb-8 lg:pb-0 lg:pr-12 text-justify"><b>Expire</b>, and require regular interaction for renewal, without adapting long-term storage funding strategies to the latest market conditions.</p>
         <p className="w-full pb-8 lg:pb-0 lg:pr-12 text-justify"><b>Are hard to verify</b> or contractually interact with from other chains.</p>
-        <p className="w-full text-justify"><b>Are expensive</b>, and promise things they can't necessarily provide. We use Filecoin service providers to provide strong storage deal guarantees in the short-term (one year) with active market management to provide for the long-term.</p>
+        <p className="w-full text-justify"><b>Are expensive</b>, and promise things they can&apos;t necessarily provide. We use Filecoin service providers to provide strong storage deal guarantees in the short-term (one year) with active market management to provide for the long-term.</p>
       </div>
       <h2>Investors and partnerships</h2>
       <p className="text-center">Building on...</p>
       <div className="flex row justify-between my-16 mx-auto w-full max-w-prose">
-        <img className="max-h-24" src="/filecoin.svg" height="96" width="96"/>
-        <img className="max-h-24" src="/compound.svg" height="96" width="96"/>
-        <img className="max-h-24" src="/yfi.svg" height="96" width="96"/>
+        <Image className="max-h-24" src="/filecoin.svg" height="96" width="96"/>
+        <Image className="max-h-24" src="/compound.svg" height="96" width="96"/>
+        <Image className="max-h-24" src="/yfi.svg" height="96" width="96"/>
       </div>
       <div className="flex row justify-between my-16 mx-auto w-full max-w-prose">
 
-        <img className="max-h-24" src="/compound.svg"height="96" width="96"/>
-        <img className="max-h-24" src="/dydx.svg" height="96" width="96"/>
-        <img className="max-h-24" src="/chainlink.svg" height="96" width="96"/>
+        <Image className="max-h-24" src="/compound.svg"height="96" width="96"/>
+        <Image className="max-h-24" src="/dydx.svg" height="96" width="96"/>
+        <Image className="max-h-24" src="/chainlink.svg" height="96" width="96"/>
       </div>
       <div className="flex row justify-around items-center my-16">
         <p className="text-2xl font-bold">Interested in investing or partnering with Banyan?</p>
         <a href="mailto:contact@banyan.computer"><button className="inline-block text-2xl font-bold px-8 my-16 border-2 p-4 border-green rounded-xl hover:bg-green transition">Get in touch</button></a>
       </div>
       <div className="text-center">
-        <a href="https://twitter.com/BanyanComputer"><i class="la-2x lab la-twitter hover:text-green"></i></a>
-        <a href="https://github.com/banyancomputer"><i class="la-2x lab la-github hover:text-green"></i></a>
+        <a href="https://twitter.com/BanyanComputer"><i className="la-2x lab la-twitter hover:text-green"></i></a>
+        <a href="https://github.com/banyancomputer"><i className="la-2x lab la-github hover:text-green"></i></a>
       </div>
       </Container>
   )
